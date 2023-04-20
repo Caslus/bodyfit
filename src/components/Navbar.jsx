dbc1592f4668696f8cde115e9a7f6929fcc9ace8
lucas philippe <lucas.philippe.nunes@gmail.com>
2023-04-20 18:07:53 -0300

    change navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,56 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaCog, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { signOut } from 'next-auth/react'
 import { useSession } from 'next-auth/react'
 
 export default function Navbar() {
   const { data: session } = useSession()
   return (
-    <div className="navbar bg-base-200">
-      <div className="flex-1">
-        <Link href="/dashboard">
-          <Image src="/logo.svg" alt="Bodyfit" width={100} height={100} />
-        </Link>
+    <div class="drawer drawer-end">
+      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col">
+        <div class="w-full navbar bg-base-300">
+          <div class="flex-1 px-2 mx-2">
+            <Link href="/dashboard">
+              <Image src="/logo.svg" alt="Bodyfit" width={100} height={100} />
+            </Link>
+          </div>
+          <div class="flex-none">
+            <label for="my-drawer-3" class="btn btn-square btn-ghost">
+              <FaBars />
+            </label>
+          </div>
+        </div>
       </div>
-      <div className="flex-none gap-2">
-        <div>
-          <p>Olá{session.user?.name ? `, ${session.user.name}` : ''}!</p>
-        </div>
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <FaBars />
-          </label>
-          <ul
-            tabIndex={0}
-            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Perfil</a>
+      <div class="drawer-side">
+        <label for="my-drawer-3" class="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 bg-base-100 justify-between">
+          <div>
+            <div className="p-4">
+              <p>Olá{session.user?.name ? `, ${session.user.name}` : ''}!</p>
+            </div>
+            <div className="divider"></div>
+            <li tabIndex={1}>
+              <a>
+                <FaUser />
+                Perfil
+              </a>
             </li>
-            <li>
-              <a>Configurações</a>
+            <li tabIndex={2}>
+              <a>
+                <FaCog />
+                Configurações
+              </a>
             </li>
-            <li>
-              <a onClick={() => signOut({ callbackUrl: '/' })}>Sair</a>
+            <li tabIndex={3}>
+              <a onClick={() => signOut({ callbackUrl: '/' })}>
+                <FaSignOutAlt /> Sair
+              </a>
             </li>
-          </ul>
-        </div>
+          </div>
+          <p className="p-4">Bodyfit</p>
+        </ul>
       </div>
     </div>
   )
